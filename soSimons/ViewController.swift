bf57223d2c7246566c884d3a987bd8d3dff9d50a
Sabrina Ouldyounes <Sabrina@utilisateurs-MacBook-Pro.local>
2017-06-12 13:23:10 -0400

    Initial Commit

---
+++ soSimons/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  soSimons
+//
+//  Created by Sabrina Ouldyounes on 17-06-12.
+//  Copyright © 2017 Sabrina Ouldyounes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
