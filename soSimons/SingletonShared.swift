dcfa066fd8a99901d0092784b51ade3ebb0fdb67
Sabrina Ouldyounes <Sabrina@utilisateurs-MacBook-Pro.local>
2017-07-11 16:27:48 -0400

    video8-FIRST STEP

--- soSimons/SingletonShared.swift
+++ soSimons/SingletonShared.swift
@@ -2,7 +2,7 @@
 import UIKit
 //============================================================
 class SingletonShared {
-    var highscore: String!
+    //var highscore: String!
     var scoreForWrongInterface: String!
     var savedHighScore = UserDefaults.standard
     static let SingletonSharedInstance = SingletonShared()
@@ -10,29 +10,50 @@ class SingletonShared {
     init() {
     
      //savedHighScore.removeObject(forKey: "score")
-     manageScore()
+     //manageScore()
+        saveDefaultIfNeeded()
     }
     
     //------------------------------------------
-    func manageScore() {
-        if let hs = savedHighScore.object(forKey: "score"){
-            highscore = String (describing: hs)
-        }else{
+//    func manageScore() {
+//        if let hs = savedHighScore.object(forKey: "score"){
+//            highscore = String (describing: hs)
+//        }else{
+//        savedHighScore.setValue("0", forKey: "score")
+//        
+//        }
+//    }
+    
+    //------------------------------------------
+    func saveDefaultIfNeeded(){
+    
+        if savedHighScore.object(forKey: "score") == nil {
         savedHighScore.setValue("0", forKey: "score")
         
         }
+    
     }
     
     //------------------------------------------
     func saveScore (_ aScore: String){
-    let a = aScore
-    let b = highscore
-        if a > b! {
+    let a = Int(aScore)
+    let b = savedHighScore.object(forKey: "score") as? String
+    let c = Int(b!)
+        
+        if a! > c! {
         savedHighScore.setValue(aScore, forKey: "score")
-        highscore = a
+       
         }
     }
      //------------------------------------------
+    
+    func resetHighscore(){
+        savedHighScore.removeObject(forKey: "score")
+        saveDefaultIfNeeded()
+    }
+    
+    
+    
 
 }
 //============================================================
--- soSimons/StartController.swift
+++ soSimons/StartController.swift
@@ -6,14 +6,35 @@ class StartController: UIViewController {
     //------------------------------------------
     override func viewDidLoad() {
         super.viewDidLoad()
-        SingletonShared.SingletonSharedInstance.manageScore()
-        highscoreLabel.text = "HIGHSCORE : \(SingletonShared.SingletonSharedInstance.highscore!)"
+        let savedScore = SingletonShared.SingletonSharedInstance.savedHighScore.object(forKey: "score")!
+        highscoreLabel.text = "HIGHSCORE : \(String(describing: savedScore))"
+        
+        
+//        SingletonShared.SingletonSharedInstance.manageScore()
+//        highscoreLabel.text = "HIGHSCORE : \(SingletonShared.SingletonSharedInstance.highscore!)"
     }
    //-------------------------------------------
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    //-------------------------------------------
+    @IBAction func resetHighscore(_ sender: UIButton) {
+        
+        highscoreLabel.text = "HIGHSCORE : 0"
+        SingletonShared.SingletonSharedInstance.resetHighscore()
+        
+        
+    }
+
+    
+    
+    
+    //-------------------------------------------
+
+    
+    
+    
 //============================================================
 
 }
